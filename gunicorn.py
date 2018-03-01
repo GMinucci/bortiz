c9d58f0b2a6cafa9e7515656b83c34c624fbc67f
Gabriel Minucci <garomi173@gmail.com>
2018-02-28 23:59:42 -0300

    Update gunicorn config file and location

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,5 @@
 import multiprocessing
 
-bind = '0.0.0.0:8000'
-
 # You can customize here to the gunicore recomended workers amount
 # workers = multiprocessing.cpu_count() * 2
-workers = 2
+workers = 1
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import os
 from src.bortiz import app as application
 
-if __name__ == "__main__":
-    port = os.environ.get('PORT', 5000)
+
+# if __name__ == "__main__":
+port = os.environ.get('PORT', 5000)
+
 application.run(port=port)
